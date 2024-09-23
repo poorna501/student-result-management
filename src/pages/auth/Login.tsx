7ad8c11d3a1690dc1c993f46a90ecfde45bc3929
poorna <poorna@marensolutions.com>
2024-09-23 09:46:05 +0530

    created high level structure

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,10 @@
+import cn from '@assets/css/Auth.module.css';
+export const Login = () => {
+  return (
+    <>
+        <div className={cn['ph-auth-cnt']}>
+            <div>my RouterProvider</div>
+        </div>
+    </>
+  );
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Login } from '../pages/auth/Login';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </>
+  )
+);
+const AppRoutes = () => <RouterProvider router={router} />;
+
+export {
+  router,
+  AppRoutes
+};
